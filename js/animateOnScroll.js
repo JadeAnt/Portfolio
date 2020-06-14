2b8d105c1afcc12e952f22ef18f51bf165484517
Jaden Antoine <jadenjantoine@gmail.com>
2020-06-14 11:15:36 -0400

    Add files via upload

---
+++ js/animateOnScroll.js
@@ -0,0 +1,17 @@
+function revealOnScroll() {
+    var scrolled = $window.scrollTop();
+    $(".revealOnScroll:not(.animated)").each(function () {
+      var $this     = $(this),
+          offsetTop = $this.offset().top;
+
+      if (scrolled + win_height_padded > offsetTop) {
+        if ($this.data('timeout')) {
+          window.setTimeout(function(){
+            $this.addClass('animated ' + $this.data('animation'));
+          }, parseInt($this.data('timeout'),10));
+        } else {
+          $this.addClass('animated ' + $this.data('animation'));
+        }
+      }
+    });
+}
